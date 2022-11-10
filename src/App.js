e092afd3afece598c163f1fbef6187470e7d0b68
kimyong8175 <kimyong8175@naver.com>
2022-11-10 18:29:25 -0500

    created table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
+import React from "react";
+import { Table } from "@mui/material";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import TableHead from "@mui/material/TableHead";
+import { TableContainer } from "@mui/material";
+import { Paper } from "@mui/material";
+
 import Customer from "./components/Customer";
 
-const customer = [
+const customers = [
   {
     id: 1,
-    image: "https://placeimg.com/640/480/any",
+    image: "https://placeimg.com/64/64/1",
     name: "Yonghyun Kim",
     birthDate: 19960629,
     gender: "Male",
@@ -12,7 +20,7 @@ const customer = [
   },
   {
     id: 2,
-    image: "https://placeimg.com/640/480/any",
+    image: "https://placeimg.com/64/64/2",
     name: "Yireh Lim",
     birthDate: 19921020,
     gender: "Male",
@@ -20,7 +28,7 @@ const customer = [
   },
   {
     id: 3,
-    image: "https://placeimg.com/640/480/any",
+    image: "https://placeimg.com/64/64/3",
     name: "Hyun Park",
     birthDate: 19850321,
     gender: "Male",
@@ -29,19 +37,38 @@ const customer = [
 ];
 
 function App() {
-  return customer.map((item) => {
-    return (
-      <Customer
-        key={item.id}
-        id={item.id}
-        image={item.image}
-        name={item.name}
-        birthDate={item.birthDate}
-        gender={item.gender}
-        job={item.job}
-      />
-    );
-  });
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Image</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Birth Date</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Job</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((item) => {
+            return (
+              <Customer
+                key={item.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                birthDate={item.birthDate}
+                gender={item.gender}
+                job={item.job}
+              />
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,35 +1,48 @@
+import React from "react";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+
 function Customer(props) {
   return (
-    <div>
-      <CustomerProfile image={props.image} name={props.name} id={props.id} />
-      <CustomerInfo
-        birthDate={props.birthDate}
-        gender={props.gender}
-        job={props.job}
-      />
-    </div>
-  );
-}
-
-function CustomerProfile(props) {
-  return (
-    <div>
-      <img src={props.image} alt="profile" />
-      <h2>
-        Name: {props.name} ({props.id})
-      </h2>
-    </div>
-  );
-}
-
-function CustomerInfo(props) {
-  return (
-    <div>
-      <p>Birth: {props.birthDate}</p>
-      <p>Gender: {props.gender}</p>
-      <p>Job: {props.job}</p>
-    </div>
+    <TableRow>
+      <TableCell>{props.id}</TableCell>
+      <TableCell>
+        <img src={props.image} alt="profile" />
+      </TableCell>
+      <TableCell>{props.name}</TableCell>
+      <TableCell>{props.birthDate}</TableCell>
+      <TableCell>{props.gender}</TableCell>
+      <TableCell>{props.job}</TableCell>
+    </TableRow>
   );
 }
 
 export default Customer;
+
+// function CustomerProfile(props) {
+//   return (
+//     <div>
+//       <img src={props.image} alt="profile" />
+//       <h2>
+//         Name: {props.name} ({props.id})
+//       </h2>
+//     </div>
+//   );
+// }
+
+// function CustomerInfo(props) {
+//   return (
+//     <div>
+//       <p>Birth: {props.birthDate}</p>
+//       <p>Gender: {props.gender}</p>
+//       <p>Job: {props.job}</p>
+//     </div>
+//   );
+//}
+
+/* <CustomerProfile image={props.image} name={props.name} id={props.id} />
+      <CustomerInfo
+        birthDate={props.birthDate}
+        gender={props.gender}
+        job={props.job}
+      /> */
